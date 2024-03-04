3434ae99ccb0a7b719ccb529840ee869ea717963
Eko748 <permanaeko74@gmail.com>
2024-03-04 22:31:55 +0700

    update connection

--- Koneksi/db_connection_dev.php
+++ Koneksi/db_connection_dev.php
@@ -1,6 +1,6 @@
 <?php
-$db = new mysqli('localhost', 'u778534470_oshs', '@Nezam123', 'u778534470_tes_oshs
-');
+$db = new mysqli('localhost', 'root', '', 'tes_oshs');
 if ($db->connect_error) {
     die("Connection failed: " . $db->connect_error);
 }
+?>
